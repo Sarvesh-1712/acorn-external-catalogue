e47c9458718992df6fe76fc31c177b62d69be703
Sarveshwaran Rajarajan <108905341+Sarvesh-1712@users.noreply.github.com>
2024-09-19 21:38:15 +1000

    Install the material ui, include styles in the home page

--- resources/ts/Pages/AcornHome.tsx
+++ resources/ts/Pages/AcornHome.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import { Inertia } from '@inertiajs/inertia';
+import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+        padding: '4',
+        textAlign: 'center',
+    },
+    button: {
+        marginTop: '6px !important'
+    }
+}));
+
 
 const AcornHome = () => {
+    const classes = useStyles();
     return (
-        <div>
-            <h1>Acorn External Catalogue system</h1>
-            <button onClick={() => Inertia.visit('/external-catalogue')}>View External Catalogues</button>
-        </div>
+        <>
+            <Box className={classes.container}>
+                <Typography variant="h4" component="h1">
+                    Welcome to Acorn's External Catalogue System
+                </Typography>
+                <Button className={classes.button} variant="contained" color="primary" onClick={(() => Inertia.visit('/external-catalogue'))}>
+                    View Catalogues
+                </Button>
+            </Box>
+        </>
     );
 };
 
