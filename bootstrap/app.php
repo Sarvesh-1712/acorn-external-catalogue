09e8546842fea3beb802c89a6be9d6f3d86cb269
Sarveshwaran Rajarajan <108905341+Sarvesh-1712@users.noreply.github.com>
2024-09-21 01:06:26 +1000

    Refactoring app to include middleware and remove kernel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,19 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->group('web', [
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ]);
+
+        $middleware->group('api', [
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\Authenticate::class,
+            \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        // TODO: custom exceptions here..
     })->create();
