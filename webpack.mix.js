fcaad0a0ff0ea026cd781538a5df6f1d7cd301c1
SarveshwaranRajarajan <sarveshwaran17rajarajan@gmail.com>
2024-09-21 10:54:15 +1000

    Testing the bloated app js with bundle analyzer. installed for checking and removed the dependencies later

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,16 @@
 const mix = require('laravel-mix');
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 mix.ts('resources/ts/app.tsx', 'public/js')
     .css('resources/css/app.css', 'public/css')
     .react()
-    .sourceMaps()
-    .version();
+    .sourceMaps();
+    /*.webpackConfig({
+        plugins: [
+            new BundleAnalyzerPlugin({
+                analyzerMode: 'static',
+                reportFilename: 'bundle-report.html',
+                openAnalyzer: false,
+            }),
+        ],
+    });*/
